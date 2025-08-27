f17e862d15a611fea10a60615e5388f4e3573e34
Hicham Agueny <h.agueny@gmail.com>
2025-08-27 14:32:12 +0200

    add my_script.sh

---
+++ my_script.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+# Set working directory and paths
+PROJECT_NBR=nn9997k
+MyWD="/cluster/work/projects/$PROJECT_NBR/$USER/llm-workshop"
+CONTAINER_DIR="${MyWD}/container"
+
+INPUT_DIR="$MyWD/data"
+
+echo "--Start copying the singularity image and base model"
+# Copy apptainer image to your work area
+cp /cluster/work/projects/nn9997k/hicham/llm-workshop/container/pytorch2.5_cu2.6.1_py3.10_custom.sif "${CONTAINER_DIR}"
+
+# Copy the base model to your work area
+cp -r /cluster/work/projects/nn9997k/hicham/llm-workshop/data/Llama-3.2-1B-Instruct ${INPUT_DIR}
+
+echo
+echo "--Start updaing config. files"
+# Update the path in your config. files
+# Fine-tuning on a single GPU
+FINETUNE_DIR="${MyWD}/fine-tuning-singlegpu"
+CONFIG_DIR="${FINETUNE_DIR}/config_scripts"
+
+cd ${CONFIG_DIR}
+sed -i "s|/cluster/projects/nn9997k/hicham/llm-workshop/data|$MyWD/data|g" "$CONFIG_DIR/1B_lora_single_device_QA.yaml"
+
+# Fine-tuning on a multiple GPUs
+FINETUNE_DIR="${MyWD}/fine-tuning-multigpu"
+CONFIG_DIR="${FINETUNE_DIR}/config_scripts"
+
+cd ${CONFIG_DIR}
+sed -i "s|/cluster/projects/nn9997k/hicham/llm-workshop/data|$MyWD/data|g" "$CONFIG_DIR/1B_lora_multi_device_QA.yaml"
+
+# Exercise
+FINETUNE_DIR="${MyWD}/exercise/fine-tuning-singlegpu"
+CONFIG_DIR="${FINETUNE_DIR}/config_scripts"
+
+cd ${CONFIG_DIR}
+sed -i "s|/cluster/projects/nn9997k/hicham/llm-workshop/data|$MyWD/data|g" "$CONFIG_DIR/1B_lora_single_device_Xsum.yaml"
+
+# Fine-tuning on a multiple GPUs
+FINETUNE_DIR="${MyWD}/exercise/fine-tuning-multigpu"
+CONFIG_DIR="${FINETUNE_DIR}/config_scripts"
+
+cd ${CONFIG_DIR}
+sed -i "s|/cluster/projects/nn9997k/hicham/llm-workshop/data|$MyWD/data|g" "$CONFIG_DIR/1B_lora_multi_device_Xsum.yaml"
+
+# Profiling
+PROFILING_DIR="${MyWD}/profiling"
+CONFIG_DIR="${PROFILING_DIR}/config_scripts"
+
+cd ${CONFIG_DIR}
+sed -i "s|/cluster/projects/nn9997k/hicham/llm-workshop/data|$MyWD/data|g" "$CONFIG_DIR/1B_lora_single_device_QA_profiling.yaml"
+
+echo "---finished :)"
+
+exit
