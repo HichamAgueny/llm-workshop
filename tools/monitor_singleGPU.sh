a98a2e0f839305a3bc5028440800dcd85bc717a3
Hicham Agueny <95568317+HichamAgueny@users.noreply.github.com>
2025-08-29 13:30:18 +0200

    Update monitor GPU

--- tools/monitor_singleGPU.sh
+++ tools/monitor_singleGPU.sh
@@ -6,7 +6,6 @@
 
 TOOLS_DIR=/cluster/work/projects/nn9997k/$USER/llm-workshop/tools
 TOOLS_BIN="${TOOLS_DIR}/bin"
-cp "${TOOLS_DIR}/gpu_format.awk" "${TOOLS_BIN}"
 
 JOB_ID="$1"
 
@@ -57,7 +56,7 @@ echo
 echo "To stop the script, press Ctrl+C"
 
 # Start monitoring specific GPU
-watch -n 1 "nvidia-smi --id=\$GPU_UUID --query-gpu=utilization.gpu,memory.used,temperature.gpu,power.draw --format=csv,noheader,nounits | awk -f \$TOOLS_BIN/gpu_format.awk"
+watch -n 1 "nvidia-smi --id=\$GPU_UUID --query-gpu=utilization.gpu,memory.used,temperature.gpu,power.draw --format=csv,noheader,nounits | awk -f \$TOOLS_DIR/gpu_format.awk"
 #INNER_EOF
 
 EOF
